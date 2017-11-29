ef1ab7c4473e9df51ecc33a5fcea7c65ee4c0020
Benjamin BOITRELLE <benjamin.boitrelle@gmail.com>
2017-11-29 16:07:41 +0100

    Add class to read chipscope file and create a vector of integer containing data lines of H0 and H1

--- src/analyseChipscopeData.cpp
+++ src/analyseChipscopeData.cpp
@@ -1,6 +1,33 @@
+//
+//  analyseChipscopeData
+//
+//  Created by Benjamin BOITRELLE on 29/11/2017.
+//
+
+
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <memory>
+#include <algorithm>
+#include <utility>
+
+#include "readChipscope.hpp"
+
 
 int main (int argc, char **argv){
+  const std::string INPUT_FILE  = "/home/ben/Documents/P2M_lightResponse/OD0.0.prn";
+
+  std::fstream myfile(INPUT_FILE);
+  std::vector<std::vector<int>> rawData;
+
+  ReadChipscope chipscopeData;
+  chipscopeData.readPrnFile(myfile, rawData);
+
+  std::cout << "Size of raw Data: " << rawData[0].size() << " and " << rawData.size() << std::endl;
+
   std::cout << "hello world" << std::endl;
   
   return 0;

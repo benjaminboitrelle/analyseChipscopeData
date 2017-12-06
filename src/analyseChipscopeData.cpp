6fe6afd72553a6e426c4888732215a8871eafe96
Benjamin BOITRELLE <benjamin.boitrelle@gmail.com>
2017-12-06 16:51:59 +0100

    Creation of class to read and process data from Chipscope and to create plot with ROOT

--- src/analyseChipscopeData.cpp
+++ src/analyseChipscopeData.cpp
@@ -1,4 +1,22 @@
+//
+//  analyseChipscopeData
+//
+//  Created by Benjamin BOITRELLE on 29/11/2017.
+//
+
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <memory>
+#include <algorithm>
+#include <utility>
+
+#include "TFile.h"
+
+#include "readChipscope.hpp"
+#include "plotHistogram.hpp"
 
 int main (int argc, char **argv){
   std::cout << "hello world" << std::endl;

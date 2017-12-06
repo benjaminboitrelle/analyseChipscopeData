06a140feb3aeb1c1e17057a825b89405e6a6c615
Benjamin BOITRELLE <benjamin.boitrelle@gmail.com>
2017-12-06 17:51:42 +0100

    Modification of readChipscope to get only one method to extract data instead of three for gain, fine and coarse

--- src/analyseChipscopeData.cpp
+++ src/analyseChipscopeData.cpp
@@ -19,7 +19,49 @@
 #include "plotHistogram.hpp"
 
 int main (int argc, char **argv){
-  std::cout << "hello world" << std::endl;
+
+// This part will be included in an external XML file
+  const std::string INPUT_FILE = "/home/ben/Documents/001/0,003,004_P2Mchp.prn";
+  std::fstream myfile(INPUT_FILE);
+  char delimiterChar = '\t';
+  std::vector<int> dataRange ={3, 47};
+  const int dataLength = 3360;
+  const int dataOffset = 9962;
+  const int sampleToRead = 2;
+  const int numberOfGainBits = 2;
+  const int numberOfFineBits = 8;
+  const int numberOfCoarseBits = 5;
+  const int rowGroupDataLength = 224;
+  const int gainBegin = 0;
+  const int gainEnd = rowGroupDataLength * numberOfGainBits; 
+  const int fineBegin = gainEnd ;
+  const int fineEnd = fineBegin + (rowGroupDataLength * numberOfFineBits);
+  const int coarseBegin = fineEnd ;
+  const int coarseEnd = coarseBegin + (rowGroupDataLength * numberOfCoarseBits);
+
+// Beginning of the analysis
+  std::vector<std::vector<int>> rawData; 
+
+  ReadChipscope chipscopeData;
+  chipscopeData.readPrnFile(myfile, delimiterChar, dataRange.at(0), dataRange.at(1), rawData);
+  std::vector<std::vector<int>> rowGroup = chipscopeData.getChipscopeDisplay(sampleToRead, dataOffset, dataLength, rawData);
+  std::vector<std::vector<int>> gainDecimals = chipscopeData.getADCInfo(rowGroup, gainBegin, gainEnd, numberOfGainBits, rowGroupDataLength);
+  std::vector<std::vector<int>> fineDecimals = chipscopeData.getADCInfo(rowGroup, fineBegin, fineEnd, numberOfFineBits, rowGroupDataLength);
+  std::vector<std::vector<int>> coarseDecimals = chipscopeData.getADCInfo(rowGroup, coarseBegin, coarseEnd, numberOfCoarseBits, rowGroupDataLength);
+
+  std::vector<std::vector<int>> gainBits = chipscopeData.getGainBits(rowGroup);
+  std::vector<std::vector<int>> fineBits = chipscopeData.getFineBits(rowGroup);
+  std::vector<std::vector<int>> coarseBits = chipscopeData.getCoarseBits(rowGroup);
+  std::vector<std::vector<int>> gainDec = chipscopeData.getGainDecimals(gainBits);
+  std::vector<std::vector<int>> fineDec = chipscopeData.getFineDecimals(fineBits);
+  std::vector<std::vector<int>> coarseDec = chipscopeData.getCoarseDecimals(coarseBits);
+
+  std::cout << "Size of gainDecimals: " << gainDecimals.size() << std::endl;
+  std::cout << "Size of gainDec: " << gainDec.size() << std::endl;
+  std::cout << "Size of fineDecimals: " << fineDecimals.size() << std::endl;
+  std::cout << "Size of fineDec: " << fineDec.size() << std::endl;
+  std::cout << "Size of coarseDecimals: " << coarseDecimals.size() << std::endl;
+  std::cout << "Size of coarseDec: " << coarseDec.size() << std::endl;
   
   return 0;
 }
---
+++ src/readChipscope.hpp
@@ -0,0 +1,53 @@
+//
+//  readChipscope.hpp
+//  analyseChipscopeData
+//
+//  Created by Benjamin BOITRELLE on 29/11/2017.
+//
+
+#ifndef readChipscope_hpp
+#define readChipscope_hpp
+
+#include <fstream>
+#include <string>
+#include <vector>
+
+
+class ReadChipscope{
+
+  public:
+    ReadChipscope();
+    ~ReadChipscope();
+
+    std::vector<int> convertStringToInt(std::vector<std::string>& inputString, std::vector<int>& output);
+    void readPrnFile(std::fstream& myFile, char delimiterChar, int beginData, int endData, std::vector<std::vector<int>> &outputImageVectorised);
+    void splitString(const std::string &inputString, char delimiterChar, std::vector<std::string> &outputVector);
+    std::vector<std::vector<int>> getChipscopeDisplay(int dataToRead, int dataStart, int dataLength, std::vector<std::vector<int>> input);
+    std::vector<std::vector<int>> getADCInfo(std::vector<std::vector<int>> input, int adcPartBegin, int adcPartEnd, int bitsLength, int rowGroupDataLength);
+
+    std::vector<std::vector<int>> getGainBits(std::vector<std::vector<int>> input);
+    std::vector<std::vector<int>> getGainDecimals(std::vector<std::vector<int>> input);
+    std::vector<std::vector<int>> getFineBits(std::vector<std::vector<int>> input);
+    std::vector<std::vector<int>> getFineDecimals(std::vector<std::vector<int>> input);
+    std::vector<std::vector<int>> getCoarseBits(std::vector<std::vector<int>> input);
+    std::vector<std::vector<int>> getCoarseDecimals(std::vector<std::vector<int>> input);
+    std::vector<std::vector<int>> prepareVectorisedImage(std::vector<std::vector<int>> input);
+    int convertBitsToDecimals(std::vector<int> inputBits);
+
+  private:
+    int m_dataLength;
+    int m_dataStart;
+    int m_chipscopeLength;
+    int m_numberOfGainBits;
+    int m_numberOfFineBits;
+    int m_numberOfCoarseBits;
+    int m_rowGroupLength;
+    int m_gainBegin;
+    int m_gainEnd;
+    int m_fineBegin;
+    int m_fineEnd;
+    int m_coarseBegin;
+    int m_coarseEnd;
+
+};
+#endif /* readChipscope_hpp */
